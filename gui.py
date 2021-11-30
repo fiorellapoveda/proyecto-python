2b56faf45c7636db7a48ca4c5777ef3491507a54
Fiorella Poveda <82069504+fiorellapoveda@users.noreply.github.com>
2021-11-30 14:49:43 -0600

    Connection between modules

--- gui.py
+++ gui.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 
 import gi
+import snake
 
 gi.require_version('Gtk', '3.0')
 from gi.repository import Gtk  # noqa
@@ -27,37 +28,37 @@ win.show_all()
 Gtk.main()
 '''
 
-
 class GUI:  # Construye el GUI a base del archivo
+
     def __init__(self, file):
         self.builder = Gtk.Builder()
         self.builder.add_from_file(file)
         self.handlers = {
             'onDestroy': Gtk.main_quit,
             'onButtonClicked': self.on_button_clicked,
-                         }
+        }
         self.builder.connect_signals(self.handlers)
 
     def start(self):
-        window = self.builder.get_object("main_window")
+        window = self.builder.get_object('main_window')
         window.show_all()
         Gtk.main()
 
     def on_button_clicked(self, button):
         id = Gtk.Buildable.get_name(button)
-        image = self.builder.get_object('image')
-        if id == 'instrucciones':
-            pass
-        elif id == 'iniciar':
-            pass
-        elif id == 'records':
-            pass
-        elif id == 'salir':
-            pass
+        print('ID asociado: {}'.format(id))
 
+        #image = self.builder.get_object('image')
+
+        if id == 'button_1':
+            snake.runGame()
+        #elif id == 'instrucciones':
+        #    pass
+        #elif id == 'records':
+        #    pass
+        #elif id == 'salir':
+        #    pass
 
 if __name__ == '__main__':
-    gtk_object = GUI(
-        '/home/luisricardo/Gladetest/template.glade'
-    )
+    gtk_object = GUI('/home/fiorella/Progra/proyecto-python/GUI.glade')
     gtk_object.start()
